42a7d2f43a6d0eec93116b69ab85244b161ae960
Jake Lewis <jake@logdyn.com>
2026-04-16 15:18:14 +0100

    rr_standalone takes args & improved behaviour

--- scripts/rr_standalone.py
+++ scripts/rr_standalone.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
-import re
+import argparse
 import requests
 import subprocess
 import sys
 
 from bs4 import BeautifulSoup
 
-linkArg = sys.stdin.read()
+def parse_args():
+    parser = argparse.ArgumentParser(description="Read Royal Road stories chapter by chapter in-terminal, via w3m.")
 
-def is_next_link(href):
-    return href and re.compile(r"/fiction/\d+/.+/(\d+)/.+").search(href)
+    parser.add_argument(
+        "url",
+        type=str,
+        help="The Royal Road chapter URL"
+    )
 
-def render(url):
+    parser.add_argument(
+        "--single-chapter", "-s",
+        action="store_true",
+        help="Single chapter only. Will not prompt to continue reading."
+    )
+
+    return parser.parse_args()
+
+def replace_prev_line(replacement = ""):
+    sys.stdout.write("\033[F") # Move cursor up one line
+    sys.stdout.write("\033[K") # Clear the line
+    if replacement:
+        print(replacement)
+
+def render(url, single_chapter = False):
     response = requests.get(url)
     response.raise_for_status()
 
@@ -20,21 +38,22 @@ def render(url):
     html = BeautifulSoup("<!DOCTYPE html><html lang='en'><head></head><body></body></html>", "html.parser")
 
     content = page.body("div", "chapter-inner chapter-content")[0]
+    keywords = page.select("meta[name=keywords]")[0]['content'].split("; ")
+    story_name = keywords[0]
+    chapter_title = keywords[2]
 
     meta = html.new_tag("meta", charset="utf-8")
     html.head.append(meta)
 
     title = html.new_tag("title")
-    title.string = page.title.string
+    title.string = story_name + " | " + chapter_title
     html.head.append(title)
     html.body.append(content)
 
-    links = page.find_all(href=is_next_link, limit = 7)
-
-    print("Reading '" + title.string + "'")
+    print("Reading\t\t- '" + title.string + "'")
 
     # w3m rendering
-    htmlStr = html.prettify()
+    html_str = html.prettify()
     p = subprocess.Popen(
         ["w3m", "-T", "text/html", "-"],
         text=True,
@@ -42,22 +61,36 @@ def render(url):
     )
 
     # Manage tty state
-    p.stdin.write(htmlStr)
+    p.stdin.write(html_str)
     p.stdin.close()
     p.wait()
     sys.stdin = open("/dev/tty")
 
-    # Manage next chapter prompt
-    if len(links) > 4:
-        answer = input("Next chapter? (Y/n): ").strip().lower() in ('', 'y', 'yes')
-        sys.stdout.write("\033[F") # Move cursor up one line
-        sys.stdout.write("\033[K") # Clear the line
-        nextLink = "https://www.royalroad.com" + links[2]["href"]
-        if answer:
-            render(nextLink)
-        else:
-            print("Finished on '" + title.string + "'")
-    else:
+    # Manage output & next chapter behaviour
+    if single_chapter:
+        sys.exit(0)
+
+    next_link = page.select("link[rel=next]")
+    read_next = False
+
+    if next_link:
+        read_next = input("Next chapter? (Y/n): ").strip().lower() in ('', 'y', 'yes')
+        replace_prev_line()
+        if read_next:
+            render("https://www.royalroad.com" + next_link[0]['href'])
+
+    if not read_next:
+        replace_prev_line("Finished on\t- '" + title.string + "'")
+
+    if not next_link:
         print("All caught up on chapters!")
 
-render(linkArg)
+if __name__ == "__main__":
+    try:
+        args = parse_args()
+        render(args.url, args.single_chapter)
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
+    
+    sys.exit(0)
